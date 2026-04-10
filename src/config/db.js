32219e9179011afaa202c33fbd9dbc629d323242
ankurchangani <changaniankur100@gmail.com>
2026-04-10 14:32:13 +0530

    DB Error slove

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,19 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dns = require("dns")
+
+dns.setServers(["1.1.1.1" , "8.8.8.8"])
+
 function connectToDB() {
+
+    console.log("MONGO_URL:", process.env.MONGO_URL); // 👈 ahi muk
 
     mongoose.connect(process.env.MONGO_URL)
         .then(() => {
-            console.log("server is connected to DB")
+            console.log("server is connected to DB");
         })
         .catch(err => {
-            console.log("Error connecting to DB")
-            process.exit(1)
-        })
-
+            console.log("error", err);
+        });
 }
 
-
-module.exports = connectToDB
+module.exports = connectToDB;
